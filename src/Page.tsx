a561b4412da56dde80f8ce80053a2a48d938c20c
hibix43 <hibix43knzw+github@gmail.com>
2021-01-10 23:18:03 +0900

    環境設定

---
+++ src/Page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Page = () => {
+  return (<p>Container!</p>);
+};
+
+ export default Page;
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Page from './Page';
+
+ReactDom.render(
+    <Page />,
+    document.getElementById('root')
+);
