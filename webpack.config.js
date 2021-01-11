79e42d4cb2cf94aab758526c23c482c5da43e34c
hibix43 <hibix43knzw+github@gmail.com>
2021-01-11 21:28:47 +0900

    webpack が期待通りに動作するようにした

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,21 @@ const path = require('path');
 module.exports = {
   entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
   },
-  resolve: ['', '.ts', '.tsx', '.js'],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  }
 };
