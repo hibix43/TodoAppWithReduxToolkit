b256e48ef8495fe4ca648704d88c91a4b806d683
hibix43 <hibix43knzw+github@gmail.com>
2021-01-24 17:02:24 +0900

    Id の作り方を変更し、削除機能を実装

--- src/ListContainer.tsx
+++ src/ListContainer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from './Button';
 import { Input } from './Input';
 import { List } from './List';
 import { ListItem } from './ListItem';
@@ -7,17 +8,28 @@ import { Todo } from './Page';
 type Props = {
   todos: Todo[];
   onChange: (id: number, completed: boolean) => void;
+  onClick: (id: number) => void;
 };
 
-export const ListContainer: React.FC<Props> = ({ todos, onChange }) => {
+export const ListContainer: React.FC<Props> = ({
+  todos,
+  onChange,
+  onClick
+}) => {
   const handleChange = (id: number) => (
     e: React.ChangeEvent<HTMLInputElement>
   ) => {
     onChange(id, e.target.checked);
   };
+  const handleClick = (id: number) => (
+    e: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onClick(id);
+  };
 
   const rows = todos.map((todo) => {
     const handleChangeId = handleChange(todo.id);
+    const handleClickId = handleClick(todo.id);
     return (
       <ListItem key={todo.id}>
         <Input
@@ -26,6 +38,9 @@ export const ListContainer: React.FC<Props> = ({ todos, onChange }) => {
           checked={todo.completed}
           onChange={handleChangeId}
         />
+        <Button type="button" onClick={handleClickId}>
+          {'削除'}
+        </Button>
       </ListItem>
     );
   });
--- src/Page.tsx
+++ src/Page.tsx
@@ -12,11 +12,13 @@ export type Todo = {
 const Page: React.FC = () => {
   const [newTodoName, setNewTodoName] = useState<string>('');
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [newId, setNewId] = useState(0);
 
   const addTodo = () => {
     if (newTodoName === '') return;
-    const newTodo = { id: todos.length, name: newTodoName, completed: false };
+    const newTodo = { id: newId, name: newTodoName, completed: false };
     setNewTodoName('');
+    setNewId(newId + 1);
     setTodos([...todos, newTodo]);
   };
 
@@ -28,6 +30,10 @@ const Page: React.FC = () => {
         changedTodo
       ])
     );
+  };
+
+  const deleteTodo = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id != id));
   };
 
   const incompleteTodos = todos.filter((todo) => !todo.completed);
@@ -43,9 +49,17 @@ const Page: React.FC = () => {
         onChange={setNewTodoName}
       />
       <Header level={2}>{`未完了: ${incompleteTodos.length} 件`}</Header>
-      <ListContainer todos={incompleteTodos} onChange={changeTodoChecked} />
+      <ListContainer
+        todos={incompleteTodos}
+        onChange={changeTodoChecked}
+        onClick={deleteTodo}
+      />
       <Header level={2}>{`完了: ${completedTodos.length} 件`}</Header>
-      <ListContainer todos={completedTodos} onChange={changeTodoChecked} />
+      <ListContainer
+        todos={completedTodos}
+        onChange={changeTodoChecked}
+        onClick={deleteTodo}
+      />
     </div>
   );
 };
